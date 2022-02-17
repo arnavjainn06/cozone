8d461f68856c02e5f4f8f10de18af1da9d0e23e6
Arnav Jain <jainarnav@outlook.com>
2022-02-18 00:17:30 +0530

    server rewritten

---
+++ functions/backControls.js
@@ -0,0 +1,30 @@
+const users = [];
+
+function newUser(id, name, room) {
+    const user = { name, id, room };
+    users.push(user);
+}
+
+function getGlobalUsers() {
+    return users;
+}
+
+function userExit(id) {
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) {
+        return users.splice(index, 1);
+    }
+}
+
+function getUsersByRoom(room) {
+    let byRoomUsers = users.filter((user) => user.room === room);
+    return byRoomUsers;
+}
+
+module.exports = {
+    newUser,
+    getGlobalUsers,
+    userExit,
+    getUsersByRoom,
+};
--- index.js
+++ index.js
@@ -1,12 +1,91 @@
-const express = require("express");
-const path = require("path");
-const http = require("http");
-
-const app = express();
-const server = http.Server(app);
-
+var express = require("express"),
+    http = require("http");
+//make sure you keep this order
+var app = express();
+var server = http.createServer(app);
 const io = require("socket.io")(server);
-const { v4: uuidV4 } = require("uuid");
+
+// const {
+//     exportUser,
+//     queryUser,
+//     userLeave,
+//     getAllUsers,
+// } = require("./functions/users");
+
+const {
+    newUser,
+    getGlobalUsers,
+    userExit,
+    getUsersByRoom,
+} = require("./functions/backControls");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Null Indexed
+app.get("/", (req, res) => {
+    res.redirect(`/create`);
+});
+
+// Creation entrypoint
+//* Render pattern: Backend > frontend > views > [EJS Engine] > Files
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+let name = null;
+
+app.get("/join/:room", (req, res) => {
+    res.render("join.ejs", { room: req.params.room });
+});
+
+app.get("/room-primary/:name/:room", (req, res) => {
+    name = req.params.name;
+    res.redirect(`/room/${req.params.room}`);
+});
+
+app.get("/room/:room", (req, res) => {
+    if (name === null) {
+        res.redirect(`/join/${req.params.room}`);
+    }
+    res.render("room.ejs", { room: req.params.room, name: name });
+    name = null;
+});
+
+io.on("connection", function (socket) {
+    let ROOM;
+
+    //* On creation of new chat room
+    socket.on("join-room", (room, name) => {
+        const user = newUser(socket.id, name, room);
+
+        socket.join(room);
+        ROOM = room;
+        io.to(room).emit("new-connection", name);
+        io.to(room).emit("users", getUsersByRoom(room));
+    });
+
+    socket.on("send", (data) => {
+        io.to(ROOM).emit("message", data);
+    });
+
+    socket.on("leaving", (name) => {
+        userExit(socket.id);
+        io.to(ROOM).emit("users", getUsersByRoom(ROOM));
+        io.to(ROOM).emit("disconnection", name);
+    });
+
+    socket.on("typing-out", (sender) => {
+        console.log(`${sender} is typing...`);
+        io.to(ROOM).emit("typing-in", sender);
+    });
+
+    socket.on("typing-stopped", () => {
+        console.log("tpying stopped!");
+        io.to(ROOM).emit("hide-typing");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("SERVER ONLINE");
+});
